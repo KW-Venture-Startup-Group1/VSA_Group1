9cd4e6e294c4641eb7ef57b1badb340c688ac5b2
JS_venture <130049769+lllilililli@users.noreply.github.com>
2023-04-13 19:50:50 +0900

    Create 12916.py

---
+++ Programmers/Week1/12916.py
@@ -0,0 +1,5 @@
+def solution(s):
+    p,y=s.count('p')+s.count('P'),s.count('y')+s.count('Y')
+    return p==y
+#p,Y의 개수가 같은 경우와 p,Y 모두 존재하지 않는 경우에 True를 리턴인데
+#p,Y가 모두 존재하지 않는 경우 역시 p와 Y의 개수가 각각 0으로 같은 상황임
