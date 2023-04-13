a7821bf58d761f2b48f9535536484766f3a5399e
JS_venture <130049769+lllilililli@users.noreply.github.com>
2023-04-13 19:33:57 +0900

    Add files via upload

---
+++ python_algorithmn/Week2/42576.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    par=participant
+    com=completion
+    answer=''
+    if par[0:len(com)]==com[0:len(com)]:
+        answer=par[len(com)]
+    else:
+        for i in range(0,len(com)):
+            if par[i]!=com[i]:
+                answer=par[i]
+                break
+    return answer
+
