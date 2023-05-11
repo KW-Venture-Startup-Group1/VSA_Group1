a4e8e029d3824bc01a760b95816c313dac536e99
JS_venture <130049769+lllilililli@users.noreply.github.com>
2023-05-11 18:43:26 +0900

    Create 12909.py

---
+++ Homework4/lessons12909/12909.py
@@ -0,0 +1,25 @@
+# 올바른 괄호 : https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+#'('으로 시작하고, ')'으로 끝날 것
+#'('와 ')'의 개수가 같을 것
+# Case 1. list
+def solution(s):
+    answer = False
+    if s=="":
+        answer = True
+    elif s[0]=='(' and s[-1]==')': 
+            if s.count('(')==s.count(')'):
+                answer = True
+    return answer
+  
+# Case 2. stack
+def solution(s):
+    stack = []
+    for element in s:
+        if element == '(':
+            stack.append(element)
+        elif stack and stack[-1] == '(':
+            stack.pop()
+        else:
+            return False
+    return len(stack) == 0 # 스택에 남아있는 값이 없으면 True
