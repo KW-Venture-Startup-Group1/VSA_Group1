5596f3661045b5f55a7c9a7566bcae14e7a55b5c
JS_venture <130049769+lllilililli@users.noreply.github.com>
2023-06-29 12:54:32 +0900

    Create 42628.py

---
+++ Homework6/lessons42628 /42628.py
@@ -0,0 +1,27 @@
+# 이중우선순위큐: https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+import heapq
+def solution(operations):
+    answer = []
+    
+    q = []
+    for operation in operations:
+        x, num = operation.split() 
+        num = int(num)
+        
+        if x == 'I':
+            heapq.heappush(q, num)
+        elif x == 'D' and num == 1:
+            if len(q) != 0:
+                max_value = max(q)
+                q.remove(max_value)
+        else:
+            if len(q) != 0:
+                heapq.heappop(q)
+    
+    if len(q) == 0:
+        answer = [0, 0]
+    else:
+        answer = [max(q), heapq.heappop(q)]
+        
+    return answer
