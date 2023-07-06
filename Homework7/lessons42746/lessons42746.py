49818fb892d0ae7a93163cb7ededae119cf18c02
UHYUNG LEE <59293156+uhyunglee@users.noreply.github.com>
2023-07-06 12:23:57 +0900

    Update lessons42746.py

--- Homework7/lessons42746/lessons42746.py
+++ Homework7/lessons42746/lessons42746.py
@@ -4,7 +4,7 @@ from functools import cmp_to_key
 def solution(numbers):
     #배열 -> 문자열
     numbers = list(map(str, numbers))
-    #내림차순 정렬 및 예외케이스 0 제거
+    #내림차순 정렬
     numbers.sort(key=cmp_to_key(lambda x, y : int(x+y) - int(y+x)), reverse=True)
-
-    return str(int(''.join(numbers)))
+    
+    return str(int(''.join(numbers))) #  예외케이스 0 제거
